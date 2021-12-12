d991781bc24ad2fc0706bd8396c5ce1d5072cf37
Zavier Divelbiss <semiviral@gmail.com>
2021-12-12 02:14:52 -0600

    make Buffer<T> indexable

--- src/opengl/buffer/mod.rs
+++ src/opengl/buffer/mod.rs
@@ -60,92 +60,13 @@ pub enum BufferTarget {
     AtomicCounter = 37568,
 }
 
-pub trait OpenGLBuffer<T: Copy>: OpenGLObject {
-    fn len(&self) -> usize;
-    fn len_mut(&mut self) -> &mut usize;
-
-    unsafe fn pin(&self, flags: MapBufferAccessFlags) -> &mut [T] {
-        assert!(self.len() > 0, "Buffer length must be >0 to be pinned.");
-        assert!(
-            (self.len() % size_of::<T>()) == 0,
-            "Buffer storage range must be aligned to `size_of::<T>()`"
-        );
-
-        let ptr = gl::MapNamedBufferRange(self.handle(), 0, self.len() as _, flags.bits());
-        std::slice::from_raw_parts_mut(ptr as *mut _, self.len() / size_of::<T>())
-    }
-
-    unsafe fn pin_range(
-        &self,
-        offset: isize,
-        length: usize,
-        flags: MapBufferAccessFlags,
-    ) -> &mut [T] {
-        assert!(
-            length <= self.len(),
-            "Range cannot exceed total buffer length."
-        );
-
-        let ptr = gl::MapNamedBufferRange(
-            self.handle(),
-            offset,
-            (length * size_of::<T>()) as isize,
-            flags.bits(),
-        );
-
-        std::slice::from_raw_parts_mut(ptr as *mut _, length)
-    }
-
-    unsafe fn unpin(&self) {
-        gl::UnmapNamedBuffer(self.handle());
-    }
-
-    fn set_data(&mut self, data: &[T], draw: BufferDraw) {
-        *self.len_mut() = data.len() * size_of::<T>();
-
-        unsafe {
-            gl::NamedBufferData(
-                self.handle(),
-                self.len() as isize,
-                data.as_ptr() as *const _,
-                draw as u32,
-            );
-
-            let slice =
-                self.pin(MapBufferAccessFlags::WRITE | MapBufferAccessFlags::INVALIDATE_BUFFER);
-
-            slice.copy_from_slice(data);
-
-            self.unpin();
-        }
-    }
-
-    fn sub_data(&mut self, offset: isize, data: &[T]) {
-        unsafe {
-            let slice = self.pin_range(
-                offset,
-                data.len(),
-                MapBufferAccessFlags::WRITE | MapBufferAccessFlags::INVALIDATE_RANGE,
-            );
-
-            slice.copy_from_slice(data);
-
-            self.unpin();
-        }
-    }
-
-    fn bind(&self, target: BufferTarget) {
-        unsafe { gl::BindBuffer(target as u32, self.handle()) };
-    }
-}
-
-pub struct Buffer<T: Copy> {
+pub struct Buffer<'s, T: Copy> {
     handle: u32,
-    len: usize,
-    marker: std::marker::PhantomData<T>,
+    data_len: usize,
+    data: Option<&'s mut [T]>,
 }
 
-impl<T: Copy> Buffer<T> {
+impl<'s, T: Copy> Buffer<'s, T> {
     pub fn new() -> Self {
         let mut handle = 0;
 
@@ -153,20 +74,19 @@ impl<T: Copy> Buffer<T> {
 
         Self {
             handle,
-            len: 0,
-            marker: std::marker::PhantomData,
+            data_len: 0,
+            data: None,
         }
     }
 
-    pub fn new_storage(len: usize, flags: BufferStorageFlags) -> Self {
-        let byte_length = len * size_of::<T>();
+    pub fn new_storage(data_len: usize, flags: BufferStorageFlags) -> Self {
         let mut buffer = Self::new();
-        buffer.len = byte_length;
+        buffer.data_len = data_len;
 
         unsafe {
             gl::NamedBufferStorage(
                 buffer.handle(),
-                byte_length as isize,
+                buffer.byte_len() as isize,
                 std::ptr::null(),
                 flags.bits(),
             )
@@ -176,14 +96,13 @@ impl<T: Copy> Buffer<T> {
     }
 
     pub fn new_data(data: &[T], draw: BufferDraw) -> Self {
-        let byte_length = data.len() * size_of::<T>();
         let mut buffer = Self::new();
-        buffer.len = byte_length;
+        buffer.data_len = data.len();
 
         unsafe {
             gl::NamedBufferData(
                 buffer.handle(),
-                byte_length as isize,
+                buffer.byte_len() as isize,
                 data.as_ptr() as _,
                 draw as u32,
             )
@@ -191,25 +110,149 @@ impl<T: Copy> Buffer<T> {
 
         buffer
     }
+
+    pub fn data_len(&self) -> usize {
+        self.data_len
+    }
+
+    pub fn byte_len(&self) -> usize {
+        self.data_len() * size_of::<T>()
+    }
+
+    fn data<'a>(&'a self) -> &'a [T] {
+        self.data
+            .as_ref()
+            .expect("Cannot use buffer data when it isn't pinned.")
+    }
+
+    fn data_mut<'a>(&'a mut self) -> &'a mut [T] {
+        self.data
+            .as_mut()
+            .expect("Cannot use buffer data when it isn't pinned.")
+    }
+
+    pub unsafe fn pin(&mut self, flags: MapBufferAccessFlags) {
+        assert!(
+            self.data_len() > 0,
+            "Buffer length must be >0 to be pinned."
+        );
+        assert!(self.data.is_none(), "Buffer has already been pinned!");
+
+        let ptr = gl::MapNamedBufferRange(self.handle(), 0, self.byte_len() as _, flags.bits());
+        self.data = Some(std::slice::from_raw_parts_mut(
+            ptr as *mut _,
+            self.data_len(),
+        ));
+    }
+
+    pub unsafe fn pin_range(
+        &mut self,
+        offset: isize,
+        data_len: usize,
+        flags: MapBufferAccessFlags,
+    ) {
+        assert!(
+            data_len <= self.data_len(),
+            "Range cannot exceed total buffer length."
+        );
+
+        let ptr = gl::MapNamedBufferRange(
+            self.handle(),
+            offset,
+            (data_len * size_of::<T>()) as isize,
+            flags.bits(),
+        );
+        self.data = Some(std::slice::from_raw_parts_mut(ptr as *mut _, data_len));
+    }
+
+    pub unsafe fn unpin(&mut self) {
+        gl::UnmapNamedBuffer(self.handle());
+        self.data = None;
+    }
+
+    pub fn resize_storage(&mut self, data_len: usize, flags: MapBufferAccessFlags) {
+        assert!(
+            self.data.is_none(),
+            "Cannot resize buffer storage while buffer is pinned."
+        );
+
+        self.data_len = data_len;
+
+        unsafe {
+            gl::NamedBufferStorage(
+                self.handle(),
+                self.byte_len() as isize,
+                std::ptr::null(),
+                flags.bits(),
+            )
+        };
+    }
+
+    pub fn set_data(&mut self, data: &[T], draw: BufferDraw) {
+        assert!(
+            self.data.is_none(),
+            "Cannot use `set_data` when buffer is pinned."
+        );
+
+        self.data_len = data.len();
+
+        unsafe {
+            gl::NamedBufferData(
+                self.handle(),
+                self.byte_len() as isize,
+                data.as_ptr() as *const _,
+                draw as u32,
+            );
+
+            self.pin(MapBufferAccessFlags::WRITE | MapBufferAccessFlags::INVALIDATE_BUFFER);
+            self.data_mut().copy_from_slice(data);
+            self.unpin();
+        }
+    }
+
+    pub fn sub_data(&mut self, offset: isize, data: &[T]) {
+        assert!(
+            self.data.is_none(),
+            "Cannot use `sub_data` when buffer is pinned."
+        );
+
+        unsafe {
+            self.pin_range(
+                offset,
+                data.len(),
+                MapBufferAccessFlags::WRITE | MapBufferAccessFlags::INVALIDATE_RANGE,
+            );
+            self.data_mut().copy_from_slice(data);
+            self.unpin();
+        }
+    }
+
+    pub fn bind(&self, target: BufferTarget) {
+        unsafe { gl::BindBuffer(target as u32, self.handle()) };
+    }
 }
 
-impl<T: Copy> crate::opengl::OpenGLObject for Buffer<T> {
+impl<T: Copy> crate::opengl::OpenGLObject for Buffer<'_, T> {
     fn handle(&self) -> u32 {
         self.handle
     }
 }
 
-impl<T: Copy> OpenGLBuffer<T> for Buffer<T> {
-    fn len(&self) -> usize {
-        self.len
-    }
+impl<T: Copy> std::ops::Index<usize> for Buffer<'_, T> {
+    type Output = T;
 
-    fn len_mut(&mut self) -> &mut usize {
-        &mut self.len
+    fn index(&self, index: usize) -> &Self::Output {
+        &self.data()[index]
     }
 }
 
-impl<T: Copy> Drop for Buffer<T> {
+impl<T: Copy> std::ops::IndexMut<usize> for Buffer<'_, T> {
+    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+        &mut self.data_mut()[index]
+    }
+}
+
+impl<T: Copy> Drop for Buffer<'_, T> {
     fn drop(&mut self) {
         unsafe { gl::DeleteBuffers(1, &raw const self.handle) };
     }
